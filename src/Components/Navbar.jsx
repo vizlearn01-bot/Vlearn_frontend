4dad9651f52dde99b5cf3c1bdeb7d77712c6c475
Griffin <omondigriffin4@gmail.com>
2025-01-09 23:05:58 +0300

    changes to the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
           open ? '' : 'rounded-full'
         }`}>
         <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-          <div className="flex flex-row items-center justify-between p-4">
+          <div className="flex flex-row items-center justify-between py-4">
             <a className="text-3xl font-bold text-white rounded-3xl focus:outline-none focus:shadow-outline">
               VizLearn<span className='text-2xl  text-custom-orange'>.</span>
             </a>
@@ -60,7 +60,12 @@ function Navbar() {
             >
              Login
             </Link>
-           
+            <Link
+              to="/register"
+              className="px-4 py-2 mx-2 text-sm bg-custom-orange text-white rounded-3xl hover:text-black focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+            >
+             Sign up
+            </Link>
           </nav>
         </div>
       </div>
