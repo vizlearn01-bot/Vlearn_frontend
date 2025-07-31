a9b79956c323c5bbf0b384422485efbfa4eaef44
Griffin <omondigriffin4@gmail.com>
2025-07-31 13:27:54 +0300

    Feat:displays data about active user

--- src/Pages/Admin/Analytics.jsx
+++ src/Pages/Admin/Analytics.jsx
@@ -1,71 +1,69 @@
 import { useEffect, useState } from "react"
 import BASE_URL from "../../config"
 import axios from "axios";
+import { Users, ListVideo , UserCheck} from "lucide-react";
 
 function Analytics() {
     const [enrolledLearners, setEnrolledLearners] = useState(0)
     const [videoCount, setVideoCount] = useState(0)
-    // Fetch course details
+    const [activeUsers, setActiveUsers] = useState(0)
+
+    //fetching analytics
     useEffect(() => {
-        const fetchUsers = async () => {
+        const fetchAnalyticsData = async () => {
             try {
-                const response = await axios.get(`${BASE_URL}/users-count/`)
-                setEnrolledLearners(response.data);
-                console.log(response.data)
-            } catch (error) {
-                console.error("Error fetching user count:", error);
+                const [userRes, videoRes, activeUsersRes] = await Promise.all([
+                    axios.get(`${BASE_URL}/users-count/`),
+                    axios.get(`${BASE_URL}/video-count/`),
+                    axios.get(`${BASE_URL}/api/subscriptions/api/subscribed-users/count/`)
+                ]);
+
+                setEnrolledLearners(userRes.data);
+                setVideoCount(videoRes.data);
+                setActiveUsers(activeUsersRes.data)
+            } catch (err) {
+                console.error("Error fetching analytics data:", err);
             }
         };
-        fetchUsers();
-        const fetchVideoCount = async () => {
-            try {
-                const response = await axios.get(`${BASE_URL}/video-count`);
-                console.log(response.data)
-                setVideoCount(response.data)
-            } catch (error) {
-                console.error(error);
-            } finally {
-            }
-        };
-        fetchVideoCount();
+
+        fetchAnalyticsData();
     }, []);
 
     return (
         <div className="p-4">
-            <h2 className="text-custom-blue text-2xl font-semibold mb-2 ">Dashboard</h2>
-            <div className="grid grid-cols-1 md:grid-cols-4 w-fit gap-10">
-                <div className="bg-custom-blue px-8 py-6 w-80 h-fit rounded-3xl">
-                    <h1 className="text-white text text-6xl font-semibold mb-4">
+            <h2 className="text-custom-blue text-3xl font-semibold mb-2 text-center md:text-left">Dashboard</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 mx-auto md:mx-0 gap-10 md:border-b border-gray-300 pb-6 w-full">
+                <div className="bg-custom-blue px-8 py-6 w-72 h-44 rounded-3xl relative">
+                    <Users strokeWidth={1} className="text-white w-12 h-12 absolute top-4 right-4" />
+                    <h1 className="text-white text-6xl font-semibold mb-8">
                         {enrolledLearners.user_count}
                     </h1>
                     <p className="text-white font-extralight tracking-wide text-xl">
                         Enrolled learners
                     </p>
                 </div>
-                <div className="bg-custom-orange px-8 py-6 w-80 h-fit rounded-3xl">
+                <div className="bg-custom-orange px-8 py-6 w-72 h-44 rounded-3xl relative">
+                    <UserCheck strokeWidth={1} className="text-white w-12 h-12 absolute top-4 right-4" />
+                    <h1 className="text-white text text-6xl font-semibold mb-8">
+                        {activeUsers.subscribed_users}
+                    </h1>
+                    <p className="text-white font-extralight tracking-wide text-xl w-full">
+                        Subscribed learners
+                    </p>
+                </div>
+
+                <div className="bg-custom-blue px-8 py-6 w-72 h-44 rounded-3xl relative">
+                    <ListVideo strokeWidth={1} className="text-white w-12 h-12 absolute top-4 right-4" />
                     <h1 className="text-white text text-6xl font-semibold mb-4">
                         {videoCount.count}
                     </h1>
-                    <p className="text-white font-extralight tracking-wide text-xl">
-                        Enrolled learners
+                    <p className="text-white font-extralight tracking-wide text-xl w-full">
+                        Available Science experiments
                     </p>
                 </div>
-                <div className="bg-custom-blue px-8 py-6 w-80 h-fit rounded-3xl">
-                    <h1 className="text-white text text-6xl font-semibold mb-4">
-                        {enrolledLearners.user_count}
-                    </h1>
-                    <p className="text-white font-extralight tracking-wide text-xl">
-                        Enrolled learners
-                    </p>
-                </div>
-                <div className="bg-custom-orange px-8 py-6 w-80 h-fit rounded-3xl">
-                    <h1 className="text-white text text-6xl font-semibold mb-4">
-                        {videoCount.count}
-                    </h1>
-                    <p className="text-white font-extralight tracking-wide text-xl">
-                        Enrolled learners
-                    </p>
-                </div>
+
+
+
             </div>
         </div>
     )
