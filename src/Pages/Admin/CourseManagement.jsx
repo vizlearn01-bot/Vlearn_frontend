135199c542678381a7f6f4d5e916e4defd56d3eb
Griffin <omondigriffin4@gmail.com>
2025-08-01 16:44:11 +0300

    Feat: creates a form for uploading questions to the platform

--- src/Pages/Admin/CourseManagement.jsx
+++ src/Pages/Admin/CourseManagement.jsx
@@ -8,16 +8,17 @@ function CourseManagement() {
   return (
     <>
       <div className="pt-4">
-        <h2 className="text-custom-blue text-3xl font-semibold mb-2 text-center md:text-left pl-4">Course management</h2>
-        <div className="flex space-x-4 mb-4">
+        <h2 className="text-custom-blue text-3xl font-semibold mb-2 text-center pl-4">Course management</h2>
+        <div className="flex space-x-4 mb-4 mx-auto justify-center">
           <button
             onClick={() => setView("questions")}
-            className="hover:underline focus:underline decoration-custom-orange underline-offset-4 decoration-2 cursor-pointer py-2 px-4">
+            className="flex items-center focus:before:content-['•'] before:text-custom-orange before:mr-2 before:font-extrabold before:text-2xl decoration-custom-orange underline-offset-4 decoration-2 cursor-pointer py-2 px-4">
             Questions
           </button>
+
           <button
             onClick={() => setView("videos")}
-            className="hover:underline focus:underline decoration-custom-orange underline-offset-4 decoration-2 cursor-pointer py-2 px-4">
+            className="flex items-center focus:before:content-['•'] before:text-custom-orange before:mr-2 before:font-extrabold before:text-2xl decoration-custom-orange underline-offset-4 decoration-2 cursor-pointer py-2 px-4">
             Videos
           </button>
         </div>
--- src/Pages/Admin/CourseManagement/QuestionManagement.jsx
+++ src/Pages/Admin/CourseManagement/QuestionManagement.jsx
@@ -1,11 +1,84 @@
 import React from 'react'
-
+import { Plus, CircleCheckBig } from 'lucide-react'
 function QuestionManagement() {
   return (
     <>
-    <div className='pl-4'>
-      This should handle Questions management
-    </div>
+      <div className='pl-10 shadow-2xl border-2 border-gray-100 rounded-3xl p-4 w-4/6 mx-auto bg-gray-50'>
+        <div className='flex p-4 items-center gap-4'>
+          <Plus strokeWidth={1.5} className='bg-custom-blue h-12 w-12 rounded-xl text-white' />
+          <div>
+            <h1 className='text-2xl font-bold'>Create new Question</h1>
+            <p className='font-light '>Add questions for students to practice</p>
+          </div>
+
+        </div>
+        <form action="">
+          <div className='grid grid-cols-2 gap-0 mb-6'>
+            <div>
+              <label htmlFor="title" className='block mb-2 '>Question title</label>
+              <input
+                type="text"
+                name='title'
+                placeholder='Enter Question title'
+                className='border border-gray-300 rounded-3xl py-2 px-4 w-2/3'
+              />
+            </div>
+            <div>
+              <label htmlFor="category" className='block mb-2'>Category</label>
+              <input
+                type="text"
+                name='category'
+                placeholder='Category'
+                className=' border border-gray-300 rounded-3xl py-2 px-4 w-2/3'
+              />
+            </div>
+          </div>
+          <label
+            htmlFor="difficulty"
+            className='block'
+          >Difficulty</label>
+          <select
+            name="difficulty"
+            id="difficulty"
+            className='w-full border border-gray-300 rounded-3xl py-3 px-4 mb-6'>
+            <option value="default">Pick a difficulty level</option>
+            <option value="hard">Hard</option>
+            <option value="medium">Medium</option>
+            <option value="easy">Easy</option>
+          </select>
+
+          <label
+            htmlFor="description"
+          >Question Description</label>
+          <textarea
+            name="description"
+            id="description"
+            className='w-full border border-gray-300 rounded-3xl h-42 py-3 px-4'
+            placeholder='Enter the full question description'></textarea>
+          <label htmlFor="answers">Answers</label>
+          <div className='space-y-4'>
+            <div className='flex items-center border border-gray-300 rounded-3xl px-4 py-2 w-2/3'>
+             <CircleCheckBig className='text-custom-orange mr-4' />
+              <input
+                type="text"
+                placeholder='answer option 1'
+                className='w-full'
+              />
+            </div>
+
+
+            <div className='flex items-center border border-gray-300 rounded-3xl px-4 py-2 w-2/3'>
+              <CircleCheckBig className='text-custom-orange mr-4' />
+              <input
+                type="text"
+                placeholder='answer option 2'
+                className='w-full '
+              />
+            </div>
+
+          </div>
+        </form>
+      </div>
     </>
   )
 }
